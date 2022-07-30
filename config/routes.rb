fdfb08cbdc54408f91d3f57dd521a0936fbdcca2
ihortok <itokaryk@gmail.com>
2022-07-30 22:54:15 +0300

    authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
+  resources :api_keys, path: 'api-keys', only: %i[index create destroy] 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
